b5cc0990ab0556bdddb156e7b5842170b814184a
Ishwar749 <19itish083@ldce.ac.in>
2024-02-25 14:21:15 +0530

    Initial commit

---
+++ src/test/java/com/ishwar/TacoCloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishwar.TacoCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
